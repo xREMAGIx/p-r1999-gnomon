dd83192de18e13b8b714e41ebfe77b48b042d6b5
xREMAGIx <tramdo2806@gmail.com>
2024-10-06 22:46:31 +0700

    :construction: setting up effects

---
+++ src/data/effects.ts
@@ -0,0 +1,10 @@
+export const EFFECTS = {
+    CEREMONIOUS: {
+        name: "Ceremonious",
+        description: "Ultimate Might **+3%** (up to 100 stacks, undispellable, disappears after casting an Ultimate).",
+    },
+    GASH: {
+        name: "Gash",
+        description: "When attacked, take additional (own ATK x20%) Genesis DMG.",
+    },
+}
